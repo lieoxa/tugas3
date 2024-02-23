f74970d0fbc6be6032e59a963e90817d0ff56f29
felix <adrielfelixposuma@gmail.com>
2024-02-23 16:32:02 +0700

    push

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@trimble-oss/modus-icons@1.9.0/dist/modus-solid/fonts/modus-icons.css">
     <link rel="stylesheet" href="css/profile.css">
@@ -15,12 +14,84 @@
 </head>
 
 <body>
+
     <div class="old">
         <div class="bg-img position-relative">
-            <img src="img/bg-profile.jpg" class="w-100 bg-profile">
+            <div class="bg-profile-img bg-profile">
+                <img src="img/bg-profile.png" class="w-100">
+            </div>
             <div class="foto-profile w-100">
-                <img src="img/profile.png" class="w-25 mx-auto foto">
-                <h5 class="text-white">Adriel Felix</h5>
+                <div class="foto-nama">
+                    <div class="kelas-foto w-100 mb-3">
+                        <img src="img/profile.png" class="w-25 foto">
+                    </div>
+                    <h2 class="text-white text-center">Brahmana Apta Putra Rehan</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container d-grid gap-3 menu">
+            <div class="accordion">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button id="profil" class="profil accordion-button collapsed position-relative rounded-top-3"
+                            type="button">
+                            <i class="modus-icons" aria-hidden="true">person</i>
+                            Edit Profil
+                            <i class="bi bi-chevron-right"></i>
+                        </button>
+                    </h2>
+                </div>
+                <div class="accordion-item rounded-bottom-3">
+                    <h2 class="accordion-header">
+                        <button id="akun"
+                            class="profil accordion-button collapsed position-relative rounded-bottom-3" type="button">
+                            <i class="modus-icons" aria-hidden="true">email</i>
+                            Edit Akun
+                            <i class="bi bi-chevron-right"></i>
+                        </button>
+                    </h2>
+                </div>
+            </div>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item rounded-top-3">
+                    <h2 class="accordion-header">
+                        <button class="profil accordion-button collapsed position-relative rounded-top-3"
+                            type="button">
+                            <i class="bi bi-bookmark-check-fill fs-5"></i>
+                            List Tontonan
+                            <i class="bi bi-chevron-right"></i>
+                        </button>
+                    </h2>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button id="laporan" class="profil accordion-button collapsed position-relative"
+                            type="button">
+                            <i class="modus-icons" aria-hidden="true">help</i>
+                            Laporkan
+                            <i class="bi bi-chevron-right"></i>
+                        </button>
+                    </h2>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="profil accordion-button collapsed position-relative" type="button">
+                            <i class="modus-icons" aria-hidden="true">key</i>
+                            Edit Sandi
+                            <i class="bi bi-chevron-right"></i>
+                        </button>
+                    </h2>
+                </div>
+                <div class="accordion-item rounded-bottom-3">
+                    <h2 class="accordion-header">
+                        <button id="log-out" class="profil accordion-button collapsed position-relative text-danger rounded-bottom-3"
+                            type="button">
+                            <i class="modus-icons" aria-hidden="true">sign_out</i>
+                            Log Out
+                            <i class="bi bi-chevron-right"></i>
+                        </button>
+                    </h2>
+                </div>
             </div>
         </div>
     </div>
@@ -54,6 +125,84 @@
             </div>
         </div>
     </footer>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    document.getElementById('profil').addEventListener('click', function() {
+
+        Swal.fire({
+            width: 369,
+            title: "Edit Profil",
+            imageUrl: "{{ asset('img/profile.png') }}",
+            input: "file",
+            imageWidth: 100,
+            imageHeight: 100,
+            imageAlt: "Custom image",
+            showCancelButton: true,
+            confirmButtonText: "Save",
+            denyButtonText: `Don't save`
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire("Saved!", "", "success");
+            }
+        })
+    });
+
+    document.getElementById('laporan').addEventListener('click', async function() {
+        const {
+            value: text
+        } = await Swal.fire({
+            input: "textarea",
+            inputLabel: "Laporkan",
+            inputPlaceholder: "Ketik laporanmu disini...",
+            inputAttributes: {
+                "aria-label": "Type your message here"
+            },
+            showCancelButton: true,
+            width: 369,
+        });
+        if (text) {
+            Swal.fire(text);
+        }
+    });
+
+    document.getElementById('log-out').addEventListener('click', async function() {
+        Swal.fire({
+            title: "Tetap ingin keluar?",
+            width: 369,
+            showDenyButton: true,
+            confirmButtonText: "Iya",
+            denyButtonText: `Tidak`
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire("logout berhasil!", "", "success");
+            } 
+        });
+
+    });
+</script>
 
 </html>
